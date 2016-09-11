f588a83a75c03e3ca0f5fdbd0e40a54f24712083
KorolyovAlexander <amkorolyov@gmail.com>
2016-09-11 23:18:02 +0300

    trying to open a picture

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ var sticker = (function(){
 	function getData(){
 		var xhr = new XMLHttpRequest();
 
-		xhr.open("GET", "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0", true);
+		xhr.open("GET", "https://pp.vk.me/c7008/v7008579/5aadc/gOZkJhRForM.jpg", true);
 		//xhr.responseType = "json";
 		xhr.onload = function(data){
 			if(xhr.readyState == 4){
