756bf8ac20d0ccc7770fb4f4f9fa19b17af2747a
KorolyovAlexander <amkorolyov@gmail.com>
2016-09-11 21:54:58 +0300

    gap 'value' from 'stickers.json' attached to 'input' instead of 'button'

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ var sticker = (function(){
 		for(var i=0; i<data.length; ++i){
 			var temp_sticker = createSticker();
 			
-			temp_sticker.children[0].value = data[i].value;
+			temp_sticker.children[1].value = data[i].value;
 			desktop.appendChild(temp_sticker);
 			temp_sticker.style.left = data[i].left;
     			temp_sticker.style.top = data[i].top;
