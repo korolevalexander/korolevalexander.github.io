c9829d9ab181d50ad8c1ba88e0e643120c1882f3
KorolyovAlexander <amkorolyov@gmail.com>
2016-09-11 23:14:47 +0300

    wiki

--- main.js
+++ main.js
@@ -77,11 +77,11 @@ var sticker = (function(){
 	function getData(){
 		var xhr = new XMLHttpRequest();
 
-		xhr.open("GET", "stickers.json", true);
-		xhr.responseType = "json";
+		xhr.open("GET", "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0", true);
+		//xhr.responseType = "json";
 		xhr.onload = function(data){
 			if(xhr.readyState == 4){
-				insertData(xhr.response);
+				console.log(xhr.response);
 			}
 		}
 		xhr.send(null);
