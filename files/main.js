e28dddbc42186f0b242a15f33f40e4c8a85386dc
KorolyovAlexander <amkorolyov@gmail.com>
2016-09-11 21:19:20 +0300

    initial commit

---
+++ files/main.js
@@ -0,0 +1,98 @@
+var sticker = (function(){
+		
+	var desktop,
+	activeSticker,
+	zIndexCount = 1,
+	shiftX,
+	shiftY,
+	divSticker = "\
+		<button class='sticker_button'>x</button>\
+		<input class='sticker_input' />\
+	";
+
+	document.addEventListener('DOMContentLoaded', init, false);
+	
+	function init(){
+		desktop = document.body;
+		desktop.addEventListener('dblclick', addSticker, false);
+		desktop.addEventListener('mousedown', mousedown, false);
+		document.addEventListener('mousemove', mousemove, false);
+		desktop.addEventListener('mouseup', mouseup, false);
+		desktop.addEventListener('click', deleteSticker, false);		
+	}
+
+	function createSticker(){
+		var temp_sticker = document.createElement('div');
+		temp_sticker.className = 'sticker_body';
+		temp_sticker.innerHTML = divSticker;
+		temp_sticker.ondragstart = function(){
+			return false;
+		};
+		
+		return temp_sticker;
+	}
+
+	function addSticker(e){
+		if(e.target == desktop)
+		{
+			var temp_sticker = createSticker();
+			
+			desktop.appendChild(temp_sticker);
+			temp_sticker.style.left = e.pageX - temp_sticker.offsetWidth / 2 + 'px';
+    			temp_sticker.style.top = e.pageY - temp_sticker.offsetHeight / 2 + 'px';
+		}
+	}
+	
+	function mousedown(e){
+		var activeElement = e.target;
+		if(activeElement.className == 'sticker_body'){
+			activeSticker = activeElement;
+			shiftX = e.pageX - parseInt(activeSticker.style.left);
+			shiftY = e.pageY - parseInt(activeSticker.style.top);
+			activeSticker.style.zIndex = zIndexCount++;
+		}
+	}
+
+	function mousemove(e){
+		if(activeSticker){
+			activeSticker.style.left = e.pageX - shiftX + 'px';
+			activeSticker.style.top = e.pageY - shiftY + 'px';
+		}
+  	}
+
+	function mouseup(){
+    		activeSticker = null;
+  	}
+
+	function deleteSticker(e){
+		var activeElement = e.target;
+		if(activeElement.className == 'sticker_button'){
+			activeElement.parentNode.remove();
+		}
+	}
+	
+	function getData(){
+		var xhr = new XMLHttpRequest();
+
+		xhr.open("GET", "files/stickers.json", true);
+		xhr.responseType = "json";
+		xhr.onload = function(data){
+			if(xhr.readyState == 4){
+				insertData(xhr.response);
+			}
+		}
+		xhr.send(null);
+	}
+	
+	function insertData(data){
+		for(var i=0; i<data.length(); ++i){
+			var temp_sticker = createSticker();
+			
+			temp_sticker.children[0].value = data[i].value;
+			desktop.appendChild(temp_sticker);
+			temp_sticker.style.left = data[i].left;
+    			temp_sticker.style.top = data[i].top;
+		}
+	}
+
+}());
